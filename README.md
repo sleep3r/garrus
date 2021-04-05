32d8c14bb6888f081068334ae585dd19a8d0fd3d
sleep3r <sleep3r@icloud.com>
2021-04-06 01:23:13 +0300

    Package main stuff

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# garrus
-In the middle of some calibrations...
+<div align="center">
+
+[![Garrus logo](https://github.com/sleep3r/pics/blob/main/garrus_pics/garrus-main.png?raw=true)](https://github.com/sleep3r/garrus)
+
+**In the middle of some calibrations...**
+
+[![CodeFactor](https://www.codefactor.io/repository/github/sleep3r/garrus/badge)](https://www.codefactor.io/repository/github/sleep3r/garrus)
+
+### Citation
+
+Please use this bibtex if you want to cite this repository in your publications:
+
+    @misc{catalyst,
+        author = {Kalashnikov, Alexander},
+        title = {Deep neaural networks calibration framework},
+        year = {2021},
+        publisher = {GitHub},
+        journal = {GitHub repository},
+        howpublished = {\url{https://github.com/sleep3r/garrus}},
+    }
---
+++ setup.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+# flake8: noqa
+# -*- coding: utf-8 -*-
+
+# Note: To use the "upload" functionality of this file, you must:
+#   $ pip install twine
+
+import io
+import os
+from shutil import rmtree
+import sys
+
+from setuptools import Command, find_packages, setup
+
+# Package meta-data.
+NAME = "garrus"
+DESCRIPTION = "Garrus. Python framework for better confidence estimate of deep neural networks."
+URL = "https://github.com/sleep3r/garrus"
+EMAIL = "sleep3r@icloud.com"
+AUTHOR = "Alexander Kalashnikov"
+REQUIRES_PYTHON = ">=3.7.0"
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+
+def load_requirements(filename):
+    """Docs? Contribution is welcome."""
+    with open(os.path.join(PROJECT_ROOT, filename), "r") as f:
+        return f.read().splitlines()
+
+
+def load_readme():
+    """Docs? Contribution is welcome."""
+    readme_path = os.path.join(PROJECT_ROOT, "README.md")
+    with io.open(readme_path, encoding="utf-8") as f:
+        return f"\n{f.read()}"
+
+
+def load_version():
+    """Docs? Contribution is welcome."""
+    context = {}
+    with open(os.path.join(PROJECT_ROOT, "garrus", "__init__.py")) as f:
+        exec(f.read(), context)
+    return context["__version__"]
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        """Docs? Contribution is welcome."""
+        pass
+
+    def finalize_options(self):
+        """Docs? Contribution is welcome."""
+        pass
+
+    def run(self):
+        """Docs? Contribution is welcome."""
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(PROJECT_ROOT, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
+
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
+
+        self.status("Pushing git tags…")
+        os.system("git tag v{0}".format(load_version()))
+        os.system("git push --tags")
+
+        sys.exit()
+
+setup(
+    name=NAME,
+    version=load_version(),
+    description=DESCRIPTION,
+    long_description=load_readme(),
+    long_description_content_type="text/markdown",
+    keywords=[
+        "Machine Learning",
+        "Distributed Computing",
+        "Deep Learning",
+        "Reinforcement Learning",
+        "Computer Vision",
+        "Natural Language Processing",
+        "Recommendation Systems",
+        "Information Retrieval",
+        "PyTorch",
+        "Confidence calibration",
+        "Confidence ranking"
+    ],
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    download_url=URL,
+    project_urls={
+        "Bug Tracker": URL,
+        "Documentation": URL,
+        "Source Code": URL,
+    },
+    packages=find_packages(exclude=("tests",)),
+    include_package_data=True,
+    license="Apache License 2.0",
+    classifiers=[
+        "Environment :: Console",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: Apache Software License",
+        # Audience
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        # Topics
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        # Programming
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    # $ setup.py publish support.
+    cmdclass={"upload": UploadCommand},
+)
